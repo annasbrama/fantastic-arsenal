a663b5d477f5a2ed2b24737a7a2243cd7215e95b
Anna <acesbrama@gmail.com>
2024-05-28 15:42:22 -0300

    Html, css, responsivo e javascript feitos

---
+++ script.js
@@ -0,0 +1,34 @@
+// Menu players
+let url = window.location.href
+
+switch(url) {
+    case'index.html':
+    window.location.href = 'goalkeepers.html'
+    break;
+    case'goalkeepers.html':
+    window.location.href = 'defenders.html'
+    break;
+    case'defenders.html':
+    window.location.href = 'midfielders.html'
+    break;
+    case'midfielders.html':
+    window.location.href = 'forwards.html'
+    break;
+    // default:
+    //     window.location.href = 'index.html'
+}
+
+//Responsive Menu
+
+let hambButton = document.querySelector("#hambButton");
+let closeButton = document.querySelector("#closeButton");
+
+let mobileMenu = document.querySelector("#mobileMenu");
+
+hambButton.addEventListener("click", function () {
+  mobileMenu.classList.add("flex");
+});
+
+closeButton.addEventListener("click", function () {
+  mobileMenu.classList.remove("flex");
+});
